10cbba2e85e9eea43bddd7f0b55f97e6e036af75
r3dc0m <jonathanvelez85@gmail.com>
2026-04-02 14:41:50 +0200

    0402.1440 App.js -> content reorg. *.css -> added variables and media query for dark mode. Initial app display formating.

--- scripts/core/App.js
+++ scripts/core/App.js
@@ -2,20 +2,21 @@ import { build } from "../utils.js";
 
 export default class App {
     constructor() {
-        this.main = null;
-        this.header = null;
-        this.content = null;
-        this.footer = null;
-        // do I need an object to load dom?
     }
 
     createLayout() {
         const body = document.body;
+
+        this.header = build("header", "", "header", body);
+        this.h1 = build("h1", "La página Hueb", "title", this.header)
+        this.nav = build("nav", "", "nav", this.header)
+
+
         this.main = build("main", "", "main", body);
-        this.header = build("header", "La página Hueb", "header", this.main);
         this.content = build("section", "", "content", this.main);
-        this.footer = build("footer", "", "footer", this.main);
-        this.p = build("p", "La página Hueb © 2026. Contenido libre de derechos | Imágenes y multimedia: pixabay.com", "my-p", this.content);
+
+        this.footer = build("footer", "", "footer", body);
+        this.footerText = build("p", "La página Hueb © 2026. Contenido libre de derechos | Imágenes y multimedia: pixabay.com", "footer-text", this.footer);
     }
 
     init() {
