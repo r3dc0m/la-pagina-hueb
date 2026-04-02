21108b181192325b558657ec3914b7ec09c0b234
r3dc0m <jonathanvelez85@gmail.com>
2026-04-02 02:21:07 +0200

    0402.0220 App.js -> dummy content + footer.

--- scripts/core/App.js
+++ scripts/core/App.js
@@ -5,14 +5,17 @@ export default class App {
         this.main = null;
         this.header = null;
         this.content = null;
+        this.footer = null;
+        // do I need an object to load dom?
     }
 
     createLayout() {
         const body = document.body;
         this.main = build("main", "", "main", body);
-        this.header = build("header", "La Página Hueb", "header", this.main);
+        this.header = build("header", "La página Hueb", "header", this.main);
         this.content = build("section", "", "content", this.main);
-        this.p = build("p", "asdf", "my-p", this.content);
+        this.footer = build("footer", "", "footer", this.main);
+        this.p = build("p", "La página Hueb © 2026. Contenido libre de derechos | Imágenes y multimedia: pixabay.com", "my-p", this.content);
     }
 
     init() {
